a388848e297183e9ba4cfac51eb75580e37d3046
Andrey Ziabko <azhub@hotmail.com>
2019-11-27 20:16:13 +0300

    create user model

--- src/main/java/pricingCalculatorTestFramework/test/UserAccessTests.java
+++ src/main/java/pricingCalculatorTestFramework/test/UserAccessTests.java
@@ -3,19 +3,22 @@ package pricingCalculatorTestFramework.test;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pricingCalculatorTestFramework.models.User;
 import pricingCalculatorTestFramework.page.GoogleHomePage;
+import pricingCalculatorTestFramework.services.UserCreator;
 
 import java.util.List;
 
-public class UserAccessTests extends TestCommonConditions {
 
+public class UserAccessTests extends TestCommonConditions {
+    User testUser = UserCreator.withCorrectCredentials();
     @Test
     public void openFreeTrialViaButtonTest() {
         List<WebElement> confirmTermsForFreeTrial =  new GoogleHomePage(driver)
                 .openPage()
                 .pressGetStartedForFreeButton()
-                .inputEmailForLoginToGoogle(USER_EMAIL)
-                .inputPasswordForLoginPageGoogle(USER_PASSWORD)
+                .inputEmailForLoginToGoogle(testUser.getEmail())
+                .inputPasswordForLoginPageGoogle(testUser.getPassword())
                 .presenceOfCheckBoxWithTerms();
         Assert.assertTrue(confirmTermsForFreeTrial.size() > 0, "Can't use for Free trial");
 
@@ -26,8 +29,8 @@ public class UserAccessTests extends TestCommonConditions {
         List<WebElement> confirmTermForFreeTrial = new GoogleHomePage(driver)
                 .openPage()
                 .pressGettingStartedLink()
-                .inputEmailForLoginToGoogle(USER_EMAIL)
-                .inputPasswordForLoginPageGoogle(USER_PASSWORD)
+                .inputEmailForLoginToGoogle(testUser.getEmail())
+                .inputPasswordForLoginPageGoogle(testUser.getPassword())
                 .pressGetTrialButton()
                 .presenceOfCheckBoxWithTerms();
         Assert.assertTrue(confirmTermForFreeTrial.size() > 0, "Can't use for Free trial");
