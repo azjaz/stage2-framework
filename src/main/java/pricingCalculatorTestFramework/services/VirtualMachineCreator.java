7957744692fa5e1a0458ef034e4b2f25acb01a37
Andrey Ziabko <azhub@hotmail.com>
2019-12-05 13:03:12 +0300

    re-write constants in caps

--- src/main/java/pricingCalculatorTestFramework/services/VirtualMachineCreator.java
+++ src/main/java/pricingCalculatorTestFramework/services/VirtualMachineCreator.java
@@ -6,29 +6,30 @@ import pricingCalculatorTestFramework.utils.XpathCreator;
 
 public class VirtualMachineCreator {
 
-    public static final String instancesNumber = TestDataConfigurator.getTestData("test.instancesNumber");
-    public static final String purposeOfVMachine = TestDataConfigurator.getTestData("test.purposeOfVMachine");
-    public static final String operatingSystem = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.operatingSystem"));
-    public static final String vmClass = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.vmClass"));
-    public static final String vmType = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.vmType"));
-    public static final String numberOfGPUs = TestDataConfigurator.getTestData("test.numberOfGPUs");
-    public static final String typeOfGPU = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.typeOfGPU"));
-    public static final String SSDsize = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.SSDsize"));
-    public static final String dataCenter = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.dataCenter"));
-    public static final String usageTerm = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.usageTerm"));
+    public static final String INSTANCES_NUMBER = TestDataConfigurator.getTestData("test.instancesNumber");
+    public static final String PURPOSE_OF_VMACHINE = TestDataConfigurator.getTestData("test.purposeOfVMachine");
+    public static final String OPERATING_SYSTEM = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.operatingSystem"));
+    public static final String VM_CLASS = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.vmClass"));
+    public static final String VM_TYPE = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.vmType"));
+    public static final String NUMBER_OF_GPUS = TestDataConfigurator.getTestData("test.numberOfGPUs");
+    public static final String TYPE_OF_GPU = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.typeOfGPU"));
+    public static final String SSD_SIZE = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.SSDsize"));
+    public static final String DATA_CENTER = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.dataCenter"));
+    public static final String USAGE_TERM = XpathCreator.createXpath(TestDataConfigurator.getTestData("test.usageTerm"));
 
     public static VirtualMachineConfig createConfiguration() {
         VirtualMachineConfig userChoiceConfig = new VirtualMachineConfig();
-                userChoiceConfig.setInstancesNumber(instancesNumber);
-                userChoiceConfig.setPurposeOfVMachine(purposeOfVMachine);
-                userChoiceConfig.setOperatingSystem(operatingSystem);
-                userChoiceConfig.setVmType(vmClass);
-                userChoiceConfig.setVmClass(vmType);
-                userChoiceConfig.setNumberOfGPUs(numberOfGPUs);
-                userChoiceConfig.setTypeOfGPU(typeOfGPU);
-                userChoiceConfig.setSSDsize(SSDsize);
-                userChoiceConfig.setDataCenter(dataCenter);
-                userChoiceConfig.setUsageTerm(usageTerm);
-        return userChoiceConfig;
+                userChoiceConfig.setInstancesNumber(INSTANCES_NUMBER);
+                userChoiceConfig.setPurposeOfVMachine(PURPOSE_OF_VMACHINE);
+                userChoiceConfig.setOperatingSystem(OPERATING_SYSTEM);
+                userChoiceConfig.setVmType(VM_TYPE);
+                userChoiceConfig.setVmClass(VM_CLASS);
+                userChoiceConfig.setNumberOfGPUs(NUMBER_OF_GPUS);
+                userChoiceConfig.setTypeOfGPU(TYPE_OF_GPU);
+                userChoiceConfig.setSSDsize(SSD_SIZE);
+                userChoiceConfig.setDataCenter(DATA_CENTER);
+                userChoiceConfig.setUsageTerm(USAGE_TERM);
+
+                return userChoiceConfig;
     }
 }
